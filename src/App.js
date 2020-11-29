996960f0621483fe6b8cd1803318282aac31e877
Govind B Chandran <govindchandran.18cs052@mbcet.ac.in>
2020-11-29 14:33:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="app__header">
+        <img src="soapboxlogo.jpg" alt=""></img>
+
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
