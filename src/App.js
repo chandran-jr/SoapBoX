d4079c91256ff628091b8b12679e2292120c0aec
Govind B Chandran <govindchandran.18cs052@mbcet.ac.in>
2020-12-20 02:33:33 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,23 +2,73 @@ import './App.css';
 import React, {useState, useEffect} from 'react';
 import Post from './Post.js';
 import {db} from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+
+
+function getModalStyle() {
+  const top = 50;
+ const left = 50;
+
+ return {
+   top: `${top}%`,
+   left: `${left}%`,
+   transform: `translate(-${top}%, -${left}%)`,
+
+ };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: 'theme.shadows[5]',
+    padding: theme.spacing(2,4,3),
+
+  },
+}));
 
 
 function App() {
 
+
+  const classes = useStyles();
   const [posts,setPosts] = useState([]);
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open,setOpen] = useState(false);
 
 
   useEffect(() => {
  
     db.collection('posts').onSnapshot(snapshot => {
-      setPosts(snapshot.docs.map(doc => doc.data()))
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+         post: doc.data()
+        
+        }
+        
+        )));
     })
 
   },[]);
 
   return (
     <div className="App">
+
+<Modal
+        open={open}
+        onClose={() => setOpen(false)}>
+
+
+      <div style={modalStyle} className={classes.paper}>
+      
+      <h2>Modal</h2>
+
+    </div>
+
+      </Modal>
 
     <div className="app__header">
 
@@ -30,9 +80,9 @@ function App() {
 
     {
 
-    posts.map(post => (
+    posts.map(({id, post}) => (
 
-      <Post username= {post.username} caption = {post.caption} imageUrl = {post.imageUrl} />
+      <Post key={id} username= {post.username} caption = {post.caption} imageUrl = {post.imageUrl} />
 
     ))
 
